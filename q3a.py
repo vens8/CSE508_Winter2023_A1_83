1cffe25f55dcbba43790f892d21f360a44fa55fb
Rahul Maddula <vensr.maddula@gmail.com>
2023-02-19 00:01:45 +0530

    Update q3a.py

--- q3a.py
+++ q3a.py
@@ -99,5 +99,45 @@ for i in range(len(kk)):
 # with open('fin.pickle','wb') as hand:
 #     pickle.dump(dictionary,hand,protocol=pickle.HIGHEST_PROTOCOL)
 
+#  Q3b
+
+
+def build_positional_index():
+    index = {}
+    for file in os.listdir():
+        if file.endswith(""):
+            file_path = f"{path}\{file}"
+            with open(file, 'r') as f:
+                line_no = 0
+                for line in f:
+                    line_no += 1
+                    words = line.strip().split()
+                    for pos, word in enumerate(words):
+                        if word not in index:
+                            index[word] = {}
+                        if file not in index[word]:
+                            index[word][file] = []
+                        index[word][file].append((line_no, pos))  # Can remove line number
+    return index
+
+
+def save_index(index, file):
+    with open(file, 'wb') as f:
+        pickle.dump(index, f)
+
+
+def load_index(file):
+    with open(file, 'rb') as f:
+        return pickle.load(f)
+
+
+index = build_positional_index()
+# print(index)
+
+
+save_index(index, indexPickle)
+loaded_index = load_index(indexPickle)
+print(loaded_index)
+
 
 
