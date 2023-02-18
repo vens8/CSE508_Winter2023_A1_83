fa1f33a4a186997b0e93650ce8bea60f73a21808
Rahul Maddula <vensr.maddula@gmail.com>
2023-02-18 23:57:56 +0530

    Update question1b.py

--- question1b.py
+++ question1b.py
@@ -1,39 +1,86 @@
-import nltk
-import string
-import re
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 import os
 
-path = r"C:\Users\91911\Desktop\CSE508_Winter2023_Dataset\CSE508_Winter2023_Dataset"
+# Preprocessing
+import string
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
+path = r"C:\Users\Rahul Maddula\PycharmProjects\IR\CSE508_Winter2023_Dataset\CSE508_Winter2023_Dataset"
 os.chdir(path)
-def remove_stop(s):
-    stop_words = set(stopwords.words("english"))
-    word_tokens = word_tokenize(s)
-    filtered_text = [word for word in word_tokens if word not in stop_words]
-    return filtered_text
 
-def remove_punc(s):
-    translator = str.maketrans('', '', string.punctuation)
-    return remove_stop(s.translate(translator))
 
-def remove_space(s):
-    return remove_punc(" ".join(s.split()))
+def updateTextFile(filePath):
+    lines, teline, tiline = [], [], []
+    a, b, c, d = 0, 0, 0, 0
+    with open(filePath, 'rt', encoding='unicode_escape') as myFile:
+        for myline in myFile:  # For each line, read to a string,
+            lines.append(myline)
+    for i in range(len(lines)):
+        if lines[i] == "<TITLE>\n":
+            a = i
+        if lines[i] == "</TITLE>\n":
+            b = i
+        if lines[i] == "<TEXT>\n":
+            c = i
+        if lines[i] == "</TEXT>\n":
+            d = i
+    for i in range(a + 1, b):
+        tiline.append(lines[i])
+    for i in range(c + 1, d):
+        teline.append(lines[i])
 
-def lowercase(s):
-    return remove_space(s.lower())
+    d1 = ""
+    for string in tiline:
+        d1 = d1 + string[0:len(string) - 1] + " "
+    for string in teline:
+        d1 = d1 + string[0:len(string) - 1] + " "
 
-def preprocessfiles(file_path):
-    with open(file_path,'r+') as myfile:
-        fin = lowercase(myfile.read())
-    with open(file_path,'w+') as myfile:
-        myfile.write(" ".join(fin))
-
+    with open(filePath, 'w+') as myFile:
+        myFile.write(d1)
+        # print(myFile.read())
 
 
 for file in os.listdir():
     if file.endswith(""):
-        file_path = f"{path}\{file}"
-        preprocessfiles(file_path)
+        filePath = f"{path}\{file}"
+        updateTextFile(filePath)
 
+
+# nltk.download('stopwords')
+# nltk.download('punkt')
+
+
+def remove_stop(s):
+    stop_words = set(stopwords.words("english"))
+    word_tokens = word_tokenize(s)
+    filtered_text = [word for word in word_tokens if word not in stop_words]
+    return remove_punc(filtered_text)
+
+
+def remove_punc(s):
+    for i in range(len(s)):
+        s[i] = s[i].translate(str.maketrans('', '', string.punctuation))
+    return remove_space(s)
+
+
+def remove_space(s):
+    for i in range(len(s)):
+        s[i] = " ".join(s[i].split())
+    return s
+
+
+def lowercase(s):
+    return remove_stop(s.lower())
+
+
+def preprocessFiles(filePath):
+    with open(filePath, 'r+') as myFile:
+        fin = lowercase(myFile.read())
+    with open(filePath, 'w+') as myFile:
+        myFile.write(" ".join(fin))
+
+
+for file in os.listdir():
+    if file.endswith(""):
+        filePath = f"{path}\{file}"
+        preprocessFiles(filePath)
